53bcd17c70c7dbcc38491e0dba556839aa99dac1
Yashwant Chauhan <yashwant_chauhan@hotmail.com>
2012-03-30 20:10:45 +0200

    Added more stuff
    + Rules window
    + Log window

--- maid-macruby/maid/RulesManager.rb
+++
@@ -1,2 +0,0 @@
-class RulesManager
-end
--- maid-macruby/maid/app.rb
+++ maid-macruby/maid/app.rb
@@ -1,18 +1,50 @@
-framework 'Cocoa'
+framework 'Foundation'
 framework 'AppKit'
 
+application = NSApplication.sharedApplication
+application.activateIgnoringOtherApps(true) 
+application.activationPolicy = NSApplicationActivationPolicyRegular
+
+copyMaidDir = `cp -R ~/.maid ~/Desktop/maidmac/maid`
+renameMaidDir = `mv .maid maid-dir`
+puts copyMaidDir + renameMaidDir
+
 def runScript(sender)
-  script = `maid -r`
-  puts script
+  script = `maid`
+  NSLog script
 end
 
-def manageRules(sender)
-  # Open RulesManager.rb
+def rules(sender)
+  frame = [100, 100, 500, 500]
+  window = NSWindow.alloc.initWithContentRect(frame,styleMask: NSTitledWindowMask | NSClosableWindowMask | NSMiniaturizableWindowMask | NSResizableWindowMask, backing: NSBackingStoreBuffered, defer: false)
+  content_view = NSView.alloc.initWithFrame(frame)  
+  window.contentView = content_view
+  window.display
+  window.orderFrontRegardless
+  window.center
+end
+
+def log(sender)
+  frame = [100, 100, 500, 500]
+  window = NSWindow.alloc.initWithContentRect(frame,styleMask: NSTitledWindowMask | NSClosableWindowMask | NSMiniaturizableWindowMask | NSResizableWindowMask, backing: NSBackingStoreBuffered, defer: false)
+  content_view = NSView.alloc.initWithFrame(frame)  
+  window.contentView = content_view
+  window.display
+  window.orderFrontRegardless
+  window.center
+  
+  logFile = File.open('maid-dir/maid.log')
+  log = logFile.read
+  
+  textView = NSTextView.alloc.initWithFrame([0,0,500,500])
+  textView.editable = false
+  textView.string = log
+  window.contentView.addSubview(textView)
 end
 
 def setupMenu
   menu = NSMenu.new
-  menu.initWithTitle 'MaidApp'
+  menu.initWithTitle 'MaidterApp'
   
   run = NSMenuItem.new
   run.title = 'Run Maid'
@@ -22,17 +54,17 @@ def setupMenu
   
   menu.addItem NSMenuItem.separatorItem
   
-  manageRules = NSMenuItem.new
-  manageRules.title = 'Manage Rules'
-  manageRules.action = 'manageRules:'
-  manageRules.target = self
-  menu.addItem manageRules
-  
   log = NSMenuItem.new
   log.title = 'Log'
   log.action = 'log:'
   log.target = self
   menu.addItem log
+  
+  rules = NSMenuItem.new
+  rules.title = 'Rules'
+  rules.action = 'rules:'
+  rules.target = self
+  menu.addItem rules
   
   menu.addItem NSMenuItem.separatorItem
   
@@ -48,7 +80,7 @@ def initStatusBar(menu)
   status_item = status_bar.statusItemWithLength(NSVariableStatusItemLength)
   status_item.setMenu menu 
   status_item.setHighlightMode(true)
-  status_item.title = 'maid'
+  status_item.title = 'm'
 end
 
 def quit(sender)
